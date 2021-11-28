0e87d4c5c0756bf200bf61c1e2c044c61be94ba1
Ryan <ryan.gharzeddine@ontariotechu.net>
2021-11-27 20:20:00 -0500

    addrecipe

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'addrecipe.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RecipeApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Sample Recipe Home Page'),
+      routes: {
+        '/addrecipe': (context) => AddRecipe(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/addrecipe');
+        },
+        tooltip: 'Create your own!',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
