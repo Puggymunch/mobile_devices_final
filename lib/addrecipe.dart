93f64fcff77848ece25a4071ef3df30c60d3c072
Ryan <ryan.gharzeddine@ontariotechu.net>
2021-11-28 16:40:10 -0500

    added some textfields

--- lib/addrecipe.dart
+++ lib/addrecipe.dart
@@ -1,10 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:camera/camera.dart';
 
-class AddRecipe extends StatelessWidget {
+List<CameraDescription> cameras = [];
+
+class AddRecipe extends StatefulWidget {
   const AddRecipe({Key? key}) : super(key: key);
 
   @override
+  State<AddRecipe> createState() => _AddRecipeState();
+}
+
+class _AddRecipeState extends State<AddRecipe> {
+  final recipes = FirebaseFirestore.instance.collection("recipes");
+  var titleController = TextEditingController();
+  var ingredientsController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create a Recipe!', style: TextStyle(fontSize: 12)),
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Title:", style: TextStyle(fontSize: 12)),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                  width: 250,
+                  child: TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                      ))),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Add Ingredients:", style: TextStyle(fontSize: 12)),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                  width: 250,
+                  child: TextField(
+                      controller: ingredientsController,
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                      ))),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          onPressed: () {
+            //addStudent();
+            Navigator.pop(context, true);
+          }),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'addrecipe.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
